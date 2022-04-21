eb9ea679276236be66cefb9fdf3c3b68b7ed3ac5
HugH-CRT <61247861+HugH-CRT@users.noreply.github.com>
2022-04-21 11:03:24 +0200

    adaptation api au serveur

--- WebServiceServer/dao/param.php
+++ WebServiceServer/dao/param.php
@@ -7,7 +7,7 @@ class Param
 	// -------------------------------------------
 	public static $user = 'HugoLePlusBo';
 	public static $pass = 'Hug@mysqlServ!2022';
-	public static $dsn = 'mysql:host=localhost;dbname=caveescape;charset=utf8';
+	public static $dsn = 'mysql:host=localhost;dbname=webservice_cours;charset=utf8';
 
 	// -------------------------------------------
 
