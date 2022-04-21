81c6495676eab3cd690fd18beb03eead6691c6eb
natoshiro40 <natoshirol@gmail.com>
2022-04-21 11:17:05 +0200

    persofiche

--- ParagonFichePerso/app/src/main/java/com/example/paragonficheperso/fichePerso.java
+++ ParagonFichePerso/app/src/main/java/com/example/paragonficheperso/fichePerso.java
@@ -3,19 +3,24 @@ package com.example.paragonficheperso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.paragonficheperso.dto.personnageDTO;
 
 public class fichePerso extends AppCompatActivity {
 
-    private TextView NomPerso = setContentView(R.id.nomPerso);
-    private Personnage perso;
+    private TextView nomPerso = findViewById(R.id.nomPerso);
+    private ImageView imagePerso = findViewById(R.id.ImagePerso);
+    private TextView descPerso = findViewById(R.id.DescriptionPersonnage);
+    private personnageDTO perso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiche_perso);
 
-        perso = (Personnage) getIntent().getSerializableExtra("Personnage");
+        perso = (personnageDTO) getIntent().getSerializableExtra("Personnage");
 
 
     }
